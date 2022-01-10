c4e92231fc69fa8e71684cbf6a92d6f64d9a7e88
yunha <wjh0970@gmail.com>
2022-01-10 13:46:48 +0900

    2nd commit

---
+++ function.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>function</h1>
+    <?php
+    $str = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum atque, minima ad ex perferendis eius illo distinctio quae excepturi? Libero, quis in! Asperiores veniam ducimus illum pariatur omnis doloremque itaque?";
+    echo $str;
+    ?>
+    <h2>strlen()</h2>
+    <?php
+    echo strlen($str);
+    ?>
+</body>
+</html>
